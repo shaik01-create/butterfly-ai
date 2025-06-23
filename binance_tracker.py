7c43c2f08c242e35049a92bcfa7237a54f28af14
shaik01-create <shaiksonu396@gmail.com>
2025-06-23 16:46:28 +0530

    Update binance_tracker.py

--- binance_tracker.py
+++ binance_tracker.py
@@ -1,1 +1,14 @@
+import os
+from binance.client import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("BINANCE_API_KEY")
+api_secret = os.getenv("BINANCE_API_SECRET")
+
+client = Client(api_key, api_secret)
+
+btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
+print(f"BTC Price: ${btc_price['price']}")
 
