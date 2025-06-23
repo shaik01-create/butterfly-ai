4fed2319c15f0527b9451df62f48ce3a72431a13
shaik01-create <shaiksonu396@gmail.com>
2025-06-23 16:45:22 +0530

    Update voice_input.py

--- voice_input.py
+++ voice_input.py
@@ -1,1 +1,13 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Listening...")
+    audio = r.listen(source)
+
+try:
+    command = r.recognize_google(audio)
+    print(f"You said: {command}")
+except:
+    print("Sorry, I couldn't understand.")
 
